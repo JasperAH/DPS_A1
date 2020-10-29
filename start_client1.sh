118995c2c195f883bef6c94d2181a57bf74cf0aa
J.A. Hasenoot <j.a.hasenoot@umail.leidenuniv.nl>
2020-10-29 09:09:39 -0700

    local split now actually spawns background tasks instead of waiting for each task to complete

--- start_client1.sh
+++ start_client1.sh
@@ -22,7 +22,7 @@ n_proc=$(nproc)
 
 for i in $(seq 1 $n_proc);
 do
-	/home/$USER/DPS_A1/start_client1_local.sh $n_reads $cid.$i "${@:3}"
+	/home/$USER/DPS_A1/start_client1_local.sh $n_reads $cid.$i "${@:3}" &
 done
 
 
